ec4d68df356009c66518347f17ca6f571b6e431a
alexelite <alex.elite@gmail.com>
2021-02-14 21:33:42 +0200

    change motes definiton

--- metrics/alex/GasMeter.js
+++ metrics/alex/GasMeter.js
@@ -1,7 +1,7 @@
 //GAS Meter
 exports.metrics = {
-  MCH : { name:'MCH', regexp:/MCH\:([\d\.]+)/i, value:'', unit:'MC/h', graph:1, mqtt:1,  graphOptions : { legendLbl:'MC/h', lines: { lineWidth:1 ,steps:true }, colors:['#09c'],  /*yaxis: { ticks: [1,5,20], transform:  function(v) {return v==0?v:Math.log(v); //log scale },*/ tickDecimals: 2} },
-  MC : { name:'MC', regexp:/MC\:([\d\.]+)/i, value:'', unit:'mc', pin:1, mqtt:1, },
+  MCH: { name: 'MCH', regexp: /MCH\:([\d\.]+)/i, value: '', unit: 'MC/h', graph: 1, mqtt: 1, graphOptions: { legendLbl: 'MC/h', lines: { lineWidth: 1, steps: true }, colors: ['#09c'],  /*yaxis: { ticks: [1,5,20], transform:  function(v) {return v==0?v:Math.log(v); //log scale },*/ tickDecimals: 2 } },
+  MC: { name: 'MC', regexp: /MC\:([\d\.]+)/i, value: '', unit: 'mc', graph: 1, mqtt: 1, graphOptions: { legendLbl: 'MC', lines: { lineWidth: 1, steps: true }, colors: ['#09c'],  /*yaxis: { ticks: [1,5,20], transform:  function(v) {return v==0?v:Math.log(v); //log scale },*/ tickDecimals: 2 } },
 }
 
 exports.events = {
